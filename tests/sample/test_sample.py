173de65d9585182c56d1d9248e5fad29e5e9b47c
Andrew S. Rosen <asrosen93@gmail.com>
2023-09-20 13:46:15 -0700

    Update test_sample.py

--- tests/sample/test_sample.py
+++ tests/sample/test_sample.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 from numpy.testing import assert_allclose
 
@@ -6,6 +7,7 @@ from template.sample import add, make_array
 
 def test_add():
 	assert add(1, 2) == 3
+	assert add(1.5, 2.0) == pytest.approx(3.5)
 
 def test_make_array():
 	assert_allclose(make_array(3), np.array([3, 3, 3]))
