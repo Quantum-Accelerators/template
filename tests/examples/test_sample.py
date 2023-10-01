d0cc2564ac44e69661e106432cc9a144ae671fc7
Andrew S. Rosen <asrosen93@gmail.com>
2023-09-30 20:02:41 -1000

    Update test_sample.py

--- tests/examples/test_sample.py
+++ tests/examples/test_sample.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 from numpy.testing import assert_allclose, assert_array_equal
 
-from template.sample import add, divide, make_array
+from template.examples.sample import add, divide, make_array
 
 
 def test_add():
