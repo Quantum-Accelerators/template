c01efcb4c7431e7db736d47c0386bc6ae6d01e28
Andrew Rosen <asrosen93@gmail.com>
2023-09-30 17:08:26 -1000

    patch

--- tests/sample/test_sample.py
+++ tests/sample/test_sample.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_array_equal
 
 from template.sample import add, divide, make_array
 
@@ -32,10 +32,9 @@ def test_make_array():
     """
     Test that the array generation works.
 
-    Note that we use `assert_allclose()` here, which is
-    effectively an element-wise call to `pytest.approx()`.
-    It wasn't strictly needed in this example but was used
-    for demonstration purposes.
+    Note that we use `assert_array_equal` to assert that two
+    numpy arrays are the same. We also use `assert_allclose()`,
+    which is effectively an element-wise call to `pytest.approx()`.
     """
-    assert make_array(3) == np.array([3, 3, 3])
+    assert_array_equal(make_array(3), np.array([3, 3, 3]))
     assert_allclose(make_array(divide(3, 2), length=4), np.array([1.5, 1.5, 1.5, 1.5]))
