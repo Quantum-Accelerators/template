79a27b791d599396cc1fd1630d1821120ce987f9
Andrew S. Rosen <asrosen93@gmail.com>
2023-09-30 19:45:17 -1000

    Add documentation (#12)
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- tests/sample/test_sample.py
+++ tests/sample/test_sample.py
@@ -1,15 +1,40 @@
 import numpy as np
 import pytest
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_array_equal
 
-from template.sample import add, make_array
+from template.sample import add, divide, make_array
 
 
 def test_add():
+    """
+    Test that the addition function works.
+    """
     assert add(1, 2) == 3
-    assert add(1.5, 2.0) == pytest.approx(3.5)
+
+
+def test_divide():
+    """
+    Test that the division function works.
+
+    Note that we use `pytest.approx()` here since 3/2 != 1.5
+    exactly due to floating point precision differences.
+
+    Also note that we can test that a given error is raised
+    by using `pytest.raises(<error>)`.
+    """
+    assert divide(3, 2) == pytest.approx(1.5)
+
+    with pytest.raises(ValueError):
+        divide(10, 0)
 
 
 def test_make_array():
-    assert_allclose(make_array(3), np.array([3, 3, 3]))
-    assert_allclose(make_array(3, 4), np.array([3, 3, 3, 3]))
+    """
+    Test that the array generation works.
+
+    Note that we use `assert_array_equal` to assert that two
+    numpy arrays are the same. We also use `assert_allclose()`,
+    which is effectively an element-wise call to `pytest.approx()`.
+    """
+    assert_array_equal(make_array(3), np.array([3, 3, 3]))
+    assert_allclose(make_array(divide(3, 2), length=4), np.array([1.5, 1.5, 1.5, 1.5]))
